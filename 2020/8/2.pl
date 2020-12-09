10aaa1fc182dae775d3341dd81ab9549062d4ceb
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2020-12-09 09:25:05 +0100

    .

--- 2020/8/2.pl
+++ 2020/8/2.pl
@@ -5,21 +5,61 @@ my $numlines=$.;
 print "nlines: $numlines\n";
 #print @lines;
 
-my $pc=0;
-my $noloop=1;
-my $acc=0;
+my $repl=0;
+my $done=0;
 
-while($pc < $numlines) {
-$lines[$pc] =~ /(\w+)\s\+?(\-?\d+)/;
-if ($hasbeen[$pc]) {print "loop!\n"; 
-exit;}
-print "$pc: $1 $2\n";
-$hasbeen[$pc]=1;
-switch($1) {
-	case "acc" { $acc += $2; }
-	case "jmp" { $pc += ($2-1); }
-	case "nop" {}
-}
-print "$npc: $1 $2 acc: $acc\n";
-$pc++;
+while(!$done)
+{
+    #her bytter vi en og en nop/jmp
+    # $repl er den vi skal bytte
+    # $replc er telleren i denne runden
+    my $replc=0;
+    my $noloop=1;
+    my $pc=0;
+
+    my $acc=0;
+    my @hasbeen;
+    while($noloop && ($pc < $numlines))
+    {
+	if ($hasbeen[$pc])
+	{
+#	    print "loop!\n"; 
+	    $noloop=0;
+	}
+	$lines[$pc] =~ /(\w+)\s\+?(\-?\d+)/;
+#	print "inner loop $pc: $1 $2 $replc $repl\n";
+	$hasbeen[$pc]=1;
+	switch($1) {
+	    case "acc" { $acc += $2; }
+	    case "jmp" { if($replc == $repl)
+			 { 
+#			     print "byttj $replc $repl\n";
+			 }
+			 else
+			 {
+#			     print "ikkebyttj $replc $repl\n";
+			     $pc += ($2-1);
+			 }
+			 $replc++;
+	    }
+	    case "nop" { if($replc == $repl)
+			 {
+#			     print "byttn $replc $repl\n";
+			     $pc += ($2-1);
+			 }
+			 else
+			 {
+#			     print "ikkebyttn $replc $repl\n";
+			 }
+			 $replc++;
+	    }
+	}
+	    $pc++;
+#	    print "inner end $pc: $1 $2 $replc $repl acc: $acc\n";
+    }
+
+    print "outer loop:  $repl $acc\n\n";
+if($pc==$numlines) {$done=1;}
+
+$repl++;
 }
