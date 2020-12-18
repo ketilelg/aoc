550d76e30260bd4affdd4b2f78899429b275e738
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2020-12-18 14:11:02 +0100

    18.2 short

--- 2020/18/2.pl
+++ 2020/18/2.pl
@@ -5,22 +5,9 @@ my $sum;
 
 while (<>)
 {
-    print $_;
+    do {} while (s/(\d+\s\+\s\d+)/eval $1/ge + s/(\([\s\d\*]+\))/eval $1/ge);
 
-    my $s;
-    do
-    {
-	$s=s/(\d+\s\+\s\d+)/eval $1/ge;
-#	print "etter eval, $s :  $_\n";
-	$s=$s+s/(\([\s\d\*]+\))/eval $1/ge;
-#	print "etter eval2, $s : $_\n";
-    } while ($s > 0);
-
-    my $res = eval;
-    $sum+=$res;
-    print "$res\n\n";;
-
+    $sum+=eval;
 }
 
 print "end: $sum\n";
-
