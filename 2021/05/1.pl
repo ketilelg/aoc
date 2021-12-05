101549215802da18703c294042387f8f805bc566
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2021-12-05 15:18:07 +0100

    .

--- 2021/05/1.pl
+++ 2021/05/1.pl
@@ -3,7 +3,7 @@
 
 my @s;
 
-sub sp
+sub sp #print (small) map
 {
     for $y (0..9)
     {
@@ -27,96 +27,47 @@ while (<>)
 {
     /(\d+),(\d+) -> (\d+),(\d+)/;
     print "linje: $1 $2 $3 $4\n";
-    if ($1 == $3)
+
+    $lx=abs($1-$3); #avstand i x
+    if ($1 != $3)
     {
-	if ($2 < $4)
-	{
-	    for $i ($2 .. $4)
+	$dx = $lx/($3-$1);
+    }
+    else
+    {
+	$dx=0;
+    } #retning i x
+
+    $ly=abs($2-$4); #avstand i y
+
+    if ($2 != $4)
+    {
+	$dy = $ly/($4-$2);
+    } else
+    {
+	$dy =0;
+    } #retning i y
+    
+    if ($lx>$ly) {$steps=$lx;} else {$steps=$ly;}
+
+    print "$lx $dx $ly $du $steps\n";
+
+    if ($lx != $ly) #kun rette
+    {
+    for $i (0 .. $steps)
 	    {
-		$s[$1][$i]++;
-		if ($s[$1][$i] == 2)
+		$s[$1+($i*$dx)][$2+($i*$dy)]++;
+		if ($s[$1+($i*$dx)][$2+($i*$dy)] == 2)
 		{
 		    $ol++;
 		    print "overlap! $1 $i \n";
 		}
 	    }
-	}
-	else
-	{
-	    for $i ($4 .. $2)
-	    {
-		$s[$1][$i]++;
-		if ($s[$1][$i] == 2)
-		{
-		    $ol++;
-		    print "overlap! $1 $i\n";
-		}
-	    }
-	}
-    }
-    elsif ($2 == $4)
-    {
-	if ($1 < $3)
-	{
-	    for $i ($1 .. $3)
-	    {
-		$s[$i][$2]++;
-		if ($s[$i][$2] == 2)
-		{
-		    $ol++;
-		    print "overlap! $i $2\n";
-		}
-	    }
-	}
-	else
-	{
-	    for $i ($3 .. $1)
-	    {
-		$s[$i][$2]++;
-		if ($s[$i][$2] == 2)
-		{
-		    $ol++;
-		    print "overlap! $i $2\n";
-		}
-	    }
-	}
-    }
-    else #diagonal
-    {
-	if ($1 < $3)
-	{
-	    $sx = $1;
-	    $num = $3 - $1;
-	}
-	else
-	{
-	    $sx = $3;
-	    $num = $1 - $3;
-	}
-
-	if ($2 < $4)
-	{
-	    $sy = $2;
-	    for $i (0 .. $num)
-	    {
-		$s[$sx+$i][$sy+$i]++;
-		if ($s[$sx+$i][$sy+$i] == 2)
-		{
-		    $ol++;
-		    print "overlap! $i $2\n";
-		}
-	    }
-
-	}
-	else
-	{
-	    $sy = $4;
-	}
-	
-    }
+    }	
     
-    print "\n";
-    sp;
+    
+#    print "\n";
+#    sp;
 }
 
 print "end: $ol\n";
