c02ba1f3c5e61e54f4383502f0346d643e314166
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2021-12-11 11:30:02 +0100

    day 10

--- 2021/10/1.pl
+++ 2021/10/1.pl
@@ -1,51 +1,86 @@
 #!/usr/bin/perl
 
+my %pm = ( '(' => ')',
+	   ')' => '(',
+	   '<' => '>',
+	   '>' => '<',
+	   '[' => ']',
+	   ']' => '[',
+	   '{' => '}',
+	   '}' => '{' );
+
+my %points = ( ')' => 3,
+	       ']' => 57,
+	       '}' => 1197,
+	       '>' => 25137 );
+
+my %cpoints = ( '(' => 1,
+		'[' => 2,
+		'{' => 3,
+		'<' => 4 );
+
+
+my $err=0;
+my $p2p=0; #part 2 points
+my @p2t;
+my $p2c=0;
+
 while(<>)
 {
     chomp;
-    print "ytre, linja: -$_-\n";
-    parse($_);    
-}
+#    print "ytre, linja: -$_-\n";
+    $p2p=parse($_);
 
-my %pm = ( "(" => ")",
-	   ")" => "(",
-	   "\<" => "\>",
-	   "\[" => "\]",
-	   "\{" => "\}");
+    if ($p2p>0)
+    {
+	$p2t[$p2c]=$p2p;
+	$p2c++;
+    }
+    
+    print "del2: $p2p\n";
+}
+    print "del1: $err\n";
+
+my @sortp2= sort { $a <=> $b } @p2t;
+
+print "p2: \n @p2t \n @sortp2 \n $sortp2[($p2c-1)/2]\n";
 
 sub parse
-#parse the line..: 
 {
     my $str=$_[0];
-#    while (true) #streng igjen
+    my @stack;
+    my $level=0;
+    foreach $c (split //, $str)
     {
-#	my $first = substr $str, 0, 1;
-	if ($str =~ /(^[\<\[\(\{])/)
+	if ($c =~ /(^[\<\[\(\{])/)
 	{
-	    print "treff, -$1-\n";
-	    my $l=$1;
-
-	    my $r=parse(substr($str,1));
-	    if ($pm{$l} == $r)
-	    {
-		print "rmatch, -$l- -".$pm{$l}."- -$r-\n";
-
-	    }
-	    else
-	    {
-		print "nomatch -$l- -$pm{$l}- -$r-\n";
-	    }
-	    #hvis venstre, husk og gå dypere uten denne.
-	    # hvis retur er "samme", så er det bra, hvis ikke, skriv ut, regn.
+	    $stack[$level]=$c;
+	    $level++;
 	}
 	else
 	{
-	    $ret=substr($str,0,1);
-	    print "høyre $ret\n";
-	    return $ret;
-	}
-    #hvis høyre: returner den.
+	    $level--;
+	    my $ll=$pm{$c};
+	    if ($ll eq $stack[$level])
+	    {
+	    }
+	    else
+	    {
+		$err+=$points{$c};
+		return;
+	    }
 
+	}
+	    
     }
+
+    my $pp=0;
+    while ($level>0)
+    {
+	$level--;
+	$pp*=5;
+	$pp+=$cpoints{$stack[$level]};
+    }
+    return $pp;
 }
 
