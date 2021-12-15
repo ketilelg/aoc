a41007b893f66053c575b180103811a648bd67ef
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2021-12-15 11:23:01 +0100

    d15, cont

--- 2021/15/1.pl
+++ 2021/15/1.pl
@@ -35,45 +35,110 @@ for my $y (0..$line)
 my @rmap; # risk from 0,0
 
 sub rr
-{ #recurse, x y, cost as input.
+{ #recurse, x y, cost, depth as input.
     my $x=$_[0];
     my $y=$_[1];
     my $c=$_[2];
+    my $rd=$_[3];
 
-    my $r=$c+$dmap[$y][$x];
-    $rmap[$y][$x]=$r;
-#    print "rr: $x $y $r\n";
-    if (($x > 0)) #vi kan se til venstre
+    if ($rd > 0)
     {
-	if ((! defined $rmap[$y][$x-1]))
+	my $r=$c+$dmap[$y][$x];
+	$rmap[$y][$x]=$r;
+	    print "rrb: $x $y $r $rd\n";
+	if (($x > 0)) #vi kan se til venstre
 	{
-	    rr($x-1,$y,$r);
+	    if ((! defined $rmap[$y][$x-1]))
+	    {
+		rr($x-1,$y,$r,$rd-1);
+	    }
+	    else
+	    {
+		if($rmap[$y][$x-1]+$dmap[$y][$x] < $rmap[$y][$x])
+		{
+		    $rmap[$y][$x]=$rmap[$y][$x-1]+$dmap[$y][$x];
+		}
+	    }
+	}
+	if (($y > 0)) #vi kan se opp
+	{
+	    if ((! defined $rmap[$y-1][$x]))
+	    {
+		rr($x,$y-1,$r,$rd-1);
+	    }
+	    else
+	    {
+		if($rmap[$y-1][$x]+$dmap[$y][$x] < $rmap[$y][$x])
+		{
+		    $rmap[$y][$x]=$rmap[$y-1][$x]+$dmap[$y][$x];
+		}
+	    }
+	}
+	if (($x < $w)) #vi kan se til høyre
+	{
+	    if ((! defined $rmap[$y][$x+1]))
+	    {
+		rr($x+1,$y,$r,$rd-1);
+	    }
+	    else
+	    {
+		if($rmap[$y][$x+1]+$dmap[$y][$x] < $rmap[$y][$x])
+		{
+		    $rmap[$y][$x]=$rmap[$y][$x+1]+$dmap[$y][$x];
+		}
+	    }
+	}
+	if (($y < $line)) #vi kan se ned
+	{
+	    if ((! defined $rmap[$y+1][$x]))
+	    {
+		rr($x,$y+1,$r,$rd-1);
+	    }
+	    else
+	    {
+		if($rmap[$y+1][$x]+$dmap[$y][$x] < $rmap[$y][$x])
+		{
+		    $rmap[$y][$x]=$rmap[$y+1][$x]+$dmap[$y][$x];
+		}
+	    }
 	}
     }
-    if (($y > 0)) #vi kan se opp
+    print "rr: $x- $y- $r- $rd\n";
+}
+
+$rmap[0][0]=0;
+rr(0,0,0,200);
+
+#iterer, se alltid til v/opp, avhengig av hva som går. 
+sub naive
+{
+    $rmap[0][0]=0;
+    for my $y (0..$line)
     {
-	if ((! defined $rmap[$y-1][$x]))
+	for my $x (0..$w)
 	{
-	    rr($x,$y-1,$r);
-	}
-    }
-    if (($x < $w)) #vi kan se til høyre
-    {
-	if ((! defined $rmap[$y][$x+1]))
-	{
-	    rr($x+1,$y,$r);
-	}
-    }
-    if (($y < $line)) #vi kan se ned
-    {
-	if ((! defined $rmap[$y+1][$x]))
-	{
-	    rr($x,$y+1,$r);
+	    if($y > 0)
+	    {
+		if($x>0)
+		{
+		    $rmap[$y][$x] = $dmap[$y][$x] + min($rmap[$y][$x-1],$rmap[$y-1][$x]);
+		}
+		else
+		{
+		    $rmap[$y][$x] = $dmap[$y][$x] + $rmap[$y-1][$x];
+		}
+		
+	    }
+	    else
+	    {
+		if($x>0)
+		{
+		    $rmap[$y][$x] = $dmap[$y][$x] + $rmap[$y][$x-1];
+		}
+	    }
 	}
     }
 }
-
-rr(0,0,0);
 
 for my $y (0..$line)
 {
