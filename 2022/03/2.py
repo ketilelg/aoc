88bfd769d1d0cd5a1adb7e424dfff929faf4d7b6
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2022-12-03 09:25:45 +0100

    d3 cleanup

--- 2022/03/2.py
+++ 2022/03/2.py
@@ -6,18 +6,17 @@ f=open('input')
 lines=f.readlines()
 
 for l in range(0,len(lines),3):
-    print("l",l)
+
     cs=set(lines[l].strip()).intersection(set(lines[l+1]).intersection(set(lines[l+2])))
-    print("cs",cs)
-    for c in cs:
-        print("forc",c)
+
+    c=''.join(cs)
 
     v=ord(c)
     if v>96:
         v-=96
     if v>64:
         v-=38
-    print("v",v)
+
     p1+=v
 
 print("p1:",p1)
