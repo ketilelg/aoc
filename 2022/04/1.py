20d23ade00e98e0bf2ea99f97bd29ca53633bbf9
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2022-12-04 10:14:39 +0100

    cleanup

--- 2022/04/1.py
+++ 2022/04/1.py
@@ -5,19 +5,12 @@ p2=0
 
 with open('input') as f:
     for line in f:
-        l,r=line.strip().split(",")
-        l1,r1=l.split("-")
-        l2,r2=r.split("-")
-        print(l1,r1,l2,r2)
-        if (int(l1)<=int(l2) and int(r1)>=int(r2)) or (int(l2)<=int(l1)) and (int(r2)>=int(r1)):
-            print("over")
+        l1,r1,l2,r2=map(int,line.strip().replace("-",",").split(","))
+
+        if (l1<=l2 and r1>=r2) or (l2<=l1 and r2>=r1):
             p1+=1
-        if (int(l1)>=int(l2) and int(l1)<=int(r2)) or (int(l2)>=int(l1)) and (int(l2)<=int(r1)):
-            print("sover")
+        if (l1>=l2 and l1<=r2) or (l2>=l1 and l2<=r1):
             p2+=1
-
-
-
 
 print("p1:",p1)
 print("p2:",p2)
