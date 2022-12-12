c7663ba47006db96a3a0e93362ebf7413a2ec9aa
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2022-12-12 12:30:39 +0100

    day 12 solution

---
+++ 2022/12/1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import sys
+sys.setrecursionlimit(3000)
+with open('input') as f:
+    map = f.read().strip().split("\n")
+
+w=len(map[0])
+h=len(map)
+
+ex = ey= sx = sy = -1
+while sx < 0:
+    sy+=1
+    sx=map[sy].find("S")
+while ex < 0:
+    ey+=1
+    ex=map[ey].find("E")
+
+map[sy] = map[sy].replace("S","`")
+map[ey] = map[ey].replace("E","{")
+
+depth= [[10000] * w for i in range(h)]
+
+def seek(x,y,v,d):
+
+    if d < depth[y][x]:
+
+        depth[y][x] = d
+
+        if map[y][x] == "`":
+            return
+
+        d+=1
+        for i in -1,1:
+            nx=x+i
+            ny=y
+            if nx >= 0 and ny >= 0 and nx < w and ny < h and d < depth[ny][nx]:
+                if ord(map[ny][nx])+1 >= ord(v):
+                    seek(nx,ny,map[ny][nx],d)
+        for i in -1,1:
+            ny=y+i
+            nx=x
+            if nx >= 0 and ny >= 0 and nx < w and ny < h and d < depth[ny][nx]:
+                if ord(map[ny][nx])+1 >= ord(v):
+                    seek(nx,ny,map[ny][nx],d)
+
+
+
+seek(ex,ey,map[ey][ex],0)
+print("p1",depth[sy][sx])
+sa=10000
+for fx in range(w):
+    for fy in range(h):
+        if map[fy][fx] == "a":
+            if depth[fy][fx] < sa:
+                sa = depth[fy][fx]
+print("p2",sa)
