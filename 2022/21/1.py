3dc9e9911f8376751442e9164accc670f887045d
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2022-12-21 11:54:51 +0100

    day 21 never ends pt. 2

--- 2022/21/1.py
+++ 2022/21/1.py
@@ -2,18 +2,17 @@
 import re
 from sympy import solve
 
-monkeys=dict(l.split(": ") for l in open("sample").read().strip().split("\n"))
+monkeys=dict(l.split(": ") for l in open("input").read().strip().split("\n"))
         
 def findeq(monkey):
-    if monkey=="humn":
-        return " h "
-    if re.search("\d+",monkeys[monkey]):
-        return monkeys[monkey]
-    else:
+    if re.search("\w+ \W \w+",monkeys[monkey]):
         l,op,r=monkeys[monkey].split(" ")
         return "(" + findeq(l) + op + findeq(r) + ")"
+    else:
+        return monkeys[monkey]
 
-h=int(monkeys["humn"])
+
 print("p1",int(eval(findeq("root"))))
-monkeys["root"]= monkeys["root"].replace("+","-")
+monkeys["root"] = monkeys["root"].replace("+","-")
+monkeys["humn"] = " h "
 print("p2:",solve(findeq("root"))[0])
