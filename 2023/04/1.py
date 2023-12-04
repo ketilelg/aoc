6039c11917eda7a93b3deef6aa44665b918e7153
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2023-12-04 13:41:22 +0100

    cleanup

--- 2023/04/1.py
+++ 2023/04/1.py
@@ -1,23 +1,20 @@
-sum=0
-sum2=0
-with open('sample2') as f:
+sum1=0
+
+with open('input') as f:
     cards = f.read().strip().split("\n")
 
 copies=[1]*(len(cards)+1)
 
 for line in cards:
-    sum2+=1
+
     lh,rh=line.split(":")
     cardno=int(lh.split()[1])
     w,c=rh.split("|")
-    wins=w.split()
-    cards=c.split()
-    nwin=len(set(wins).intersection(cards))
+    nwin=len(set(w.split()) & set(c.split()))
     if(nwin>0):
-        sum+=2**(nwin-1)
+        sum1+=2**(nwin-1)
         for i in range(nwin):
             copies[i+cardno+1]+=copies[cardno]
-            sum2+=copies[cardno]
 
-print("1:", sum)
-print("2:", sum2)
+print("1:", sum1)
+print("2:", sum(copies)-1)
