3edacb52b7b497945ef53fe8dc97386a552c7a61
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2023-12-06 09:35:58 +0100

    cleanup d6

--- 2023/06/solve.py
+++ 2023/06/solve.py
@@ -8,21 +8,21 @@ dists=list(map(int,inp[1].split(":")[1].split()))
 
 s1=1
 
-for (t,d) in zip(times,dists):
+def ntimes(t,d):
     x1=math.ceil((-t + math.sqrt((t*t) - (4 * d))) / -2)
     x2=math.floor((-t - math.sqrt((t*t) - (4 * d))) / -2)
-    nn=x2-x1+1
-    s1 *= nn
+    return(x2-x1+1)
+    
+
+for (t,d) in zip(times,dists):
+    s1 *= ntimes(t,d)
 
 print("1:", s1)
         
 t=int("".join(list(map(str,times))))
 d=int("".join(list(map(str,dists))))
-x1=math.floor((-t + math.sqrt((t*t) - (4 * d))) / -2)
-x2=math.ceil((-t - math.sqrt((t*t) - (4 * d))) / -2)
-s2=x2-x1-1
 
-print("2:", s2)
+print("2:", ntimes(t,d))
 
 
 
