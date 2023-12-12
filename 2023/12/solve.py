a22c48c9493e3efca3185f5f28d0bff6da16e6a3
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2023-12-12 15:19:26 +0100

    towards pt 2

--- 2023/12/solve.py
+++ 2023/12/solve.py
@@ -10,68 +10,63 @@ with open(sys.argv[1] if (len(sys.argv) == 2) else 'sample') as f:
     lines = ((s.split(" ")) for s in  f.read().strip().split("\n"))
 
 
-def br(dds,ggg,c):
-    ds=dds
-    gg=ggg
-    print("br",ds,gg)
-
-    # matcher lista med tallene?
-    lc =(list(map(len,gg)))
-    print("lc",lc,ds)
-    if(lc==ds):
-        print("match")
-        return 1
+def repl(ss,ds,pos):
+    str=ss
+    m=0
 
     
-    if(gg[0].find("?") >=0):
-        print("q")
-        # iterer over alle mulige gg[0]:
-        
-        
-    else:
-        print("hash")
-        return(br(ds[1:],gg[1:],c))
-       
-       
-#    for g in gg:
-#        print("g",g)
-#        if(g.find("?") >= 0):
-#            print("q")
-#            for 
-#        else:
-#            print("hash",g,l)
-#            if(
-    return 0
-
-def repl(str,ds,pos):
-    m=0
     if(pos< len(str)):
+
         if(str[pos]=="?"):
+
             for r in ".#":
-                m+=repl(str.replace("?",r,1),ds,pos+1)
+                nstr=str.replace("?",r,1)
+                lc =(list(map(len,(g for g in str[:pos].split(".") if (g)))))
+#                print("lcds",lc,ds,nstr)
+                if(lc[:max(0,len(lc)-1)]==ds[:max(0,len(lc)-1)]):
+                    m+=repl(nstr,ds,pos+1)
+#                else:
+#                    print("cut")
         else:
             m+=repl(str,ds,pos+1)
     else:
+        lc =(list(map(len,(g for g in str[:pos].split(".") if (g)))))
 #        print("str",str)
         
         # matcher lista med tallene?
-        lc =(list(map(len,(g for g in str.split(".") if (g)))))
 #        print("lc",lc,ds)
         if(lc==ds):
-#            print("match")
+#            print("match",str)
             m=1
         else:
             m=0
     return(m)
 
 
+
 for l in lines:
-#    print()
-#    print("L",l)
+    print()
+    print("L",l)
 
     ds=(list(map(int,l[1].split(","))))
-    s1+=repl(l[0],ds,0)
-    print("result:",s1)
+    r=repl(l[0],ds,0)
+#    print("res",r)
+#    gjør noe ala "test for med #/. foran/bak, multipliser resultatene.
+#    (.) skiller, så no problem, # kan koples til foran og/eller bak,
+#    må tenkes
+
+
+#    l2=l[0]+"?"+l[0]+"?"+l[0]+"?"+l[0]+"?"+l[0]
+#    ds2=ds*5
+#    print("l2ds2",l2,ds2)
+#    r2=repl(l2,ds2,0)
+#    print("res2",r2)
+#    s2+=r2
+
+
+    s1+=r
+
+                    
         
 #    gg=[g for g in l[0].split(".") if(g)]
 #    br(ds,gg,0)
