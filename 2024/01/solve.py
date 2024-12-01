d8372945b3e20c708475b341b75aaf9810d992c5
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2024-12-01 09:23:42 +0100

    day 1

--- 2024/01/solve.py
+++ 2024/01/solve.py
@@ -4,24 +4,19 @@ import sys
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     inp = f.read().strip().split("\n")
 
+ll=[]
+rl=[]
+for li in inp:
+    l,r=li.split("   ")
+    ll.append(int(l))
+    rl.append(int(r))
+ll.sort()
+rl.sort()
+d=0
+dd=0
+for i in range(len(ll)):
+    d+=abs(ll[i]-rl[i])
+    dd+=ll[i]*rl.count(ll[i])
 
-# times=list(map(int,inp[0].split(":")[1].split()))
-# dists=list(map(int,inp[1].split(":")[1].split()))
-
-
-# def ntimes(t,d):
-#    x1=math.ceil((-t + math.sqrt((t*t) - (4 * d))) / -2)
-#    x2=math.floor((-t - math.sqrt((t*t) - (4 * d))) / -2)
-#    return(x2-x1+1)
-    
-# s1= math.prod(list(map(ntimes,times,dists)))
-    
-#print("1:", s1)
-        
-#t=int("".join(list(map(str,times))))
-#d=int("".join(list(map(str,dists))))
-
-#print("2:", ntimes(t,d))
-
-
-
+print("1:",d)
+print("2:",dd)
