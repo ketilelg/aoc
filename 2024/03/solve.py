5b6f085a2d76c97ee63410dd82e66a266c6696f5
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2024-12-03 09:27:11 +0100

    cleanup

--- 2024/03/solve.py
+++ 2024/03/solve.py
@@ -8,14 +8,14 @@ p1=0
 p2=0
 
 do=True
-matches=re.findall(r"((mul)\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don\'t\(\))",inp)
+matches=re.findall(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don\'t\(\))",inp)
 for x in matches:
-    if(x[1] == "mul"):
-        p1+=int(x[2])*int(x[3])
+    if(x[0] == "mul"):
+        p1+=int(x[1])*int(x[2])
         if do:
-            p2+=int(x[2])*int(x[3])
+            p2+=int(x[1])*int(x[2])
     else:
-        do = x[4] == "do()"
+        do = x[3] == "do()"
 
 print("1:",p1)
 print("2:",p2)
