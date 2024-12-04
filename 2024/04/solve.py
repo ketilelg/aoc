fa5bcd437059697ef9e10afa34060d1e0fca919e
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2024-12-04 22:43:15 +0100

    cleanup more

--- 2024/04/solve.py
+++ 2024/04/solve.py
@@ -9,8 +9,8 @@ p2=0
 w=len(map)
 h=len(map[0])
 
-for y in range(len(map)):
-    for x in range(len(map[0])):
+for y in range(w):
+    for x in range(h):
         if(map[y][x]=="X"):
             for dx in (-1,0,1):
                 for dy in (-1,0,1):
@@ -22,13 +22,11 @@ for y in range(len(map)):
                         p1+=1
 
     
-for x in range(1,len(map[0])-1):
-    for y in range(1,len(map)-1):
+for x in range(1,w-1):
+    for y in range(1,h-1):
         if(map[y][x]=="A"):
-            if (((map[y-1][x-1]=="M" and map[y+1][x+1]=="S") or
-                 (map[y-1][x-1]=="S" and map[y+1][x+1]=="M")) and
-                ((map[y+1][x-1]=="M" and map[y-1][x+1]=="S") or
-                 (map[y+1][x-1]=="S" and map[y-1][x+1]=="M"))):
+            if (map[y-1][x-1] + map[y+1][x+1] in ["MS","SM"] and
+                map[y+1][x-1] + map[y-1][x+1] in ["MS","SM"]):
                 p2+=1
 
 
