977f01019510a876e72f524959097af594edd489
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2024-12-11 07:56:36 +0100

    cleanup

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -2,20 +2,20 @@ import sys
 from functools import cache
 
 with open(sys.argv[1] if (len(sys.argv) > 1) else 'input') as f:
-    inp = list(f.read().strip().split(" "))
+    inp = list(map(int,f.read().strip().split(" ")))
 
 @cache
 def blink(num,gen):
-    if(gen<=0):
+    if(gen==0):
         return 1
     else:
-        if num=="0":
-            return blink("1",gen-1)
-        elif len(num)%2 == 0:
-            return(blink(str(int(num[:int(len(num)/2)])),gen-1)+
-                   blink(str(int(num[int(len(num)/2):])),gen-1))
+        if num==0:
+            return blink(1,gen-1)
+        elif len(str(num))%2 == 0:
+            return(blink(int(str(num)[:len(str(num))//2]),gen-1)+
+                   blink(int(str(num)[len(str(num))//2:]),gen-1))
         else:
-            return(blink(str(2024*int(num)),gen-1))
+            return(blink(2024*num,gen-1))
             
 def solve(gens):
     r=0
