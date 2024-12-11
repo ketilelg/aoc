98e94cd013297be61e855ffb0d5776e6139c79a7
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2024-12-11 07:33:25 +0100

    part2. memoize for teh win

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -1,32 +1,27 @@
 import sys
-import copy
+from functools import cache
 
-with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
+with open(sys.argv[1] if (len(sys.argv) > 1) else 'input') as f:
     inp = list(f.read().strip().split(" "))
 
-p1=p2=0
-
+@cache
 def blink(num,gen):
-    global p1
-    if(gen==25):
-        p1+=1
+    if(gen<=0):
+        return 1
     else:
-#        print("bb",num,gen,len(num),gen)
         if num=="0":
-            blink("1",gen+1)
+            return blink("1",gen-1)
         elif len(num)%2 == 0:
-            blink(str(int(num[:int(len(num)/2)])),gen+1)
-            blink(str(int(num[int(len(num)/2):])),gen+1)
+            return(blink(str(int(num[:int(len(num)/2)])),gen-1)+
+                   blink(str(int(num[int(len(num)/2):])),gen-1))
         else:
-            blink(str(2024*int(num)),gen+1)
+            return(blink(str(2024*int(num)),gen-1))
             
+def solve(gens):
+    r=0
+    for i in inp:
+        r+=blink(i,gens)
+    return r
 
-
-for i in inp:
-    print("i",i)
-    blink(i,0)
-
-
-
-print("1:",p1)
-print("2:",p2)
+print("1:",solve(25))
+print("2:",solve(75))
