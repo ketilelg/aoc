f247d87b8c24db10bb65577fef59a2f6f43eb453
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2024-12-13 12:04:38 +0100

    part2

--- 2024/13/solve.py
+++ 2024/13/solve.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import numpy as np
 
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     machines = f.read().strip().split("\n\n")
@@ -7,46 +8,28 @@ with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
 
 p1=p2=0
 
-def presses(ax,bx,ay,by,resx,resy):
-    if(bx*3 > ax): #b most expensive
-        na=nb=rr=0
-        found=False
-        rr=(-1,-1)
-        while(not found and na*ax<resx):
-            if (resx-(na*ax))%bx == 0 and (resy-(na*ay))%by==0:
-#                print("hit",na,(resx-na*ax)//bx)
-                # result!
-                found=True
-                rr=(na,(resx-na*ax)//bx)
-            else:
-                na+=1
-        return(rr)
+def npresses(ax,bx,ay,by,resx,resy):
+    # 2 equations, 2 unknowns..:
+    A=np.array([[ax,bx],
+                [ay,by]])
+    y = np.array([resx,resy])
+    x=np.linalg.solve(A,y)
+    a=round(x[0],3) #teh ugly, because finite precision
+    b=round(x[1],3)
+    if(a.is_integer() and b.is_integer()):
+        return int((a*3) + b)
     else:
-        na=nb=rr=0
-        found=False
-        rr=(-1,-1)
-        while(not found and nb*bx<resx):
-            if (resx-(nb*bx))%ax == 0 and (resy-(nb*by))%ay == 0:
-#                print("hit",nb,(resx-nb*bx)//ax)
-                # result!
-                found=True
-                rr=((resx-nb*bx)//ax,nb)
-            else:
-                nb+=1
-        return(rr)
-            
+        return 0
 
 
 
 for m in machines:
-    res=re.findall("\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)",m)
+    res=re.findall(r"\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)",m)
     ax,ay,bx,by,px,py=map(int,res[0])
-    pp=presses(ax,bx,ay,by,px,py)
-    print("ff",pp)
-    if(pp!=(-1,-1)):
-        p1+=(pp[0]*3)+pp[1]
-
+    p1+=npresses(ax,bx,ay,by,px,py)
+    px+=10000000000000
+    py+=10000000000000
+    p2+=npresses(ax,bx,ay,by,px,py)
 
 print("1:",p1)
-# 79660 too high. 
 print("2:",p2)
