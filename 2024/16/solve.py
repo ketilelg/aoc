531895a472ed102b1b74ca535b59a2bd2c285e2d
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2024-12-16 10:25:28 +0100

    day2. works but slow

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -18,9 +18,18 @@ def printmap(m):
             print(c,end="")
         print("")
 
+def printscore(m):
+    for l in m:
+        for d in "<^>v":
+            print(d," ",end="")
+            for c in l:
+                print("|%7d"%c[d],end="")
+            print("|")
+        print("")
+
 startx=starty=goalx=goaly=0
 visited = [[False]*w for i in range(h)]
-costs = [[maxcost]*w for i in range(h)]
+costs = [[{"<":maxcost,"^":maxcost,">":maxcost,"v":maxcost} for j in range(w)] for i in range(h)]
 
 for y in range(w):
     for x in range(h):
@@ -39,47 +48,45 @@ dv={"<":(-1,0),"^":(0,-1),">":(1,0),"v":(0,1)}
 rsets=[]
 
 def mazerun(x,y,dir,score,path):
-#    print("mr",x,y,dir,score)
     global p1,maxcost,visited
-#    if maze[y][x]=="E":
-#        print("end: ",score)
-#        return
+    visited[y][x]=True
     for d in "<^>v":
         sc=0
-        if costs[y][x]>score:
-            costs[y][x]=score
+        if costs[y][x][dir]>score:
+            costs[y][x][dir]=score
         nx=x+dv[d][0]
         ny=y+dv[d][1]
         if d != dir:
             sc+=1000
-        if not visited[ny][nx] and maze[ny][nx]!="E" and costs[ny][nx] >= score+sc:
-            visited[ny][nx]=True
-#            maze[ny][nx]=d
+        if not visited[ny][nx] and maze[ny][nx]=="." and score+sc+1 < p1 and costs[ny][nx][d] > score+sc:
+#            visited[ny][nx]=True
             mazerun(nx,ny,d,score+sc+1,path|{(nx,ny)})
-            visited[ny][nx]=False
-#            maze[ny][nx]="."
+#            visited[ny][nx]=False
         if maze[ny][nx]=="E":
-            print("end: ",score+1,len(path),path)
-            if score+1<p1:
+            print("end: ",score+1,len(path))
+            if score+1<=p1:
                 p1=score+1
-#            printmap(maze)
-            rsets.append((score+1,path.copy()))
-            return
-
-printmap(maze)
-#printmap(visited)
-print("gg",startx,starty,goalx,goaly)
+                rsets.append((score+1,path.copy())) #remember this path. possible winning
+    visited[y][x]=False
 
 mazerun(startx,starty,">",0,{(startx,starty)})
 
+# printscore(costs)
 
-print("sdfsdf",len(rsets))
+# print("sdfsdf",len(rsets))
 
 seats=set()
 for rs in rsets:
     if rs[0]==p1:
+#        print("rs",rs[1])
         seats=seats|rs[1]
+
+for s in seats:
+    maze[s[1]][s[0]]="O"
+
+printmap(maze)
+
 print("1:",p1)
-print("2:",len(seats))
+print("2:",len(seats)+1)
 # 540 too low
 # 644 too low
