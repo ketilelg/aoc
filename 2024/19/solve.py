c121fa55dbb652e0963316937c10db3164b2e8d6
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2024-12-19 12:43:07 +0100

    faster pt2

--- 2024/19/solve.py
+++ 2024/19/solve.py
@@ -4,25 +4,33 @@ from functools import cache
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     inp = f.read().strip().split("\n\n")
 
-towels=inp[0].split(", ")
+towels=set(inp[0].split(", "))
 designs=inp[1].split("\n")
 
 p1=0
 p2=0
 
 @cache
-def designok(design):
+def designok(design,towelmin,towelmax):
     r=0
-    for t in towels:
-        if design.startswith(t):
-            if len(t)==len(design):
-                r += 1 #result!
+    for i in range(towelmin,min(towelmax,len(design))+1):
+        if design[:i] in towels:
+            if i==len(design):
+                r+=1
             else:
-                r = r + designok(design[len(t):])
+                r+= designok(design[i:],towelmin,towelmax)
+    # for t in towels:
+    #     if design.startswith(t):
+    #         if len(t)==len(design):
+    #             r += 1 #result!
+    #         else:
+    #             r = r + designok(design[len(t):],towelmin,towelmax)
     return r
 
+towelmax=max(map(len,towels))
+towelmin=min(map(len,towels))
 for d in designs:
-    r=designok(d)
+    r=designok(d,towelmin,towelmax)
     if r>0:
         p1+=1
     p2+=r
