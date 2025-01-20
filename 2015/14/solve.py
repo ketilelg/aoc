083b820be5096069f96f73b79e7350abf66cad98
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2025-01-20 22:26:53 +0100

    day14 p1+2

---
+++ 2015/14/solve.py
@@ -0,0 +1,49 @@
+import sys
+from collections import defaultdict
+
+with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
+    inp = f.read().strip().split("\n")
+
+p1=p2=0
+
+
+limit=2503
+#limit=1000
+
+birdp={}
+birdd={}
+dirsc={}
+for b in inp:
+    ll=b.split(" ")
+    name=ll[0]
+    birdp[name]=0
+    dirsc[name]=0
+    speed=int(ll[3])
+    flight=int(ll[6])
+    rest=int(ll[13])
+    birdd[name]=(speed,flight,rest)
+    print("sdf",speed,flight,rest)  
+    print("ht",limit//(flight+rest),(limit//(flight+rest))*speed)
+    dist=((limit//(flight+rest))*speed*flight)+(speed*(min(limit%(flight+rest),flight)))
+    print("sdf",speed,flight,rest,dist)  
+    if dist>p1:
+        p1=dist
+print("bbb",birdp,birdd)
+
+for i in range(1,limit):
+    for b in birdp:
+        speed,flight,rest=birdd[b]
+        birdp[b]=((i//(flight+rest))*speed*flight)+(speed*(min(i%(flight+rest),flight)))
+
+    maxb=""
+    maxd=0
+    for b in birdp:
+        if birdp[b]>maxd:
+            maxd=birdp[b]
+    for b in birdp:
+        if birdp[b]==maxd:
+            dirsc[b]+=1
+
+print("sdf",dirsc)
+print("1:",p1) #4390 too high
+print("2:",p2)
