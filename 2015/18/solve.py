ccb6e5f39bec9b48642d01114060c6d23daa19dc
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2025-01-24 10:17:09 +0100

    d18

--- 2015/18/solve.py
+++ 2015/18/solve.py
@@ -24,19 +24,40 @@ def ngen(m):
                 for yy in range((max(0,y-1)),min(h,y+2)):
                     if m[yy][xx]=="#":
                         nn+=1
-            if nn==4 or (m[y][x]=="#" and nn==3):
+#                    print("xy",x,y,xx,yy,nn)
+            if (m[y][x]=="." and nn==3) or (m[y][x]=="#" and (nn==3 or nn==4)):
                 new[y][x]="#"
             else:
                 new[y][x]="."
+#            print(nn,end="")
+#        print()
     return new
                             
 p1=p2=0
-w=len(inp[0])
-h=len(inp)
-print("sdf",w,h,inp)
-printmap(inp)
-i=ngen(inp)
-print("sdfsdf")
-printmap(i)
-print("1:",p1)
-print("2:",p2)
+
+p1inp=deepcopy(inp)
+for i in range(100):
+    p1inp=ngen(p1inp)
+#    printmap(inp)
+
+for i in range(100):
+    inp[0][0]="#"
+    inp[-1][0]="#"
+    inp[-1][-1]="#"
+    inp[0][-1]="#"
+    inp=ngen(inp)
+
+inp[0][0]="#"
+inp[-1][0]="#"
+inp[-1][-1]="#"
+inp[0][-1]="#"
+
+def countit(inp):
+    r=0
+    for x in range(len(inp[0])):
+        for y in range(len(inp)):
+            if inp[y][x]=="#":
+                r+=1
+    return r
+print("1:",countit(p1inp)) 
+print("2:",countit(inp))
