1e25916b18acba78f3bb3ee6adf0918c8e784613
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2025-01-27 09:40:59 +0100

    d19p2

--- 2015/19/solve.py
+++ 2015/19/solve.py
@@ -1,6 +1,5 @@
 import sys
 import re
-from collections import defaultdict
 
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     ii,molecule = f.read().strip().split("\n\n")
@@ -10,38 +9,21 @@ for rule in ii.split("\n"):
     l,r=rule.split(" => ")
     rules.append((l,r))
 
-# restr="|".join(rules)
-
-# print("rr",restr,rules)
 p1=p2=0
 
 newm=set()
 
 for rr in rules:
     r,l=rr
-#    print("rule",r,l)
     for i in re.finditer(r,molecule):
         s,e=i.span()
-#        print("i",molecule[:s]+l+molecule[e:])
         newm.add(molecule[:s]+l+molecule[e:])
 
-print("1:",len(newm)) #615 too high 
-
-def myf(i):
-    return len(i[1])
-
-rules.sort(key=myf)
+print("1:",len(newm)) 
 
 while molecule != "e":
-    for rr in rules:
-        l,r=rr
-#        print("rule",r,l)
-        for i in re.finditer(r,molecule):
-            s,e=i.span()
-            print("i",molecule[:s]+l+molecule[e:])
-            mm=molecule[:s]+l+molecule[e:]
-            molecule=mm
-            p2+=1
-    print("molecule",molecule)
+    for l,r in rules:
+        molecule,nn=re.subn(r,l,molecule)
+        p2+=nn
 
 print("2:",p2)
