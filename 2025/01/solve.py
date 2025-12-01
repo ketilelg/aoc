b757c39d51a5300e38664c682c13d2ec6b9f7741
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2025-12-01 13:52:21 +0100

    shorter code

--- 2025/01/solve.py
+++ 2025/01/solve.py
@@ -10,25 +10,18 @@ p1=p2=0
 pos=50
 for l in inp:
     v=int(l[1:])
-    pp=pos
     if l[0]=="L":
         pos-=v
     else:
         pos+=v
 
-    if(pos>100):
-        p2+=abs(int(pos/100))
-        if pos%100==0:
-            p2-=1
-    if pos<0:
-        p2+=abs(int(pos/100))
-        if pp!=0 and pos%100!=0:
+    p2+=abs(int(pos/100))
+    if pos<=0 and pos!=-v:
             p2+=1
 
     pos=pos%100
     if pos==0:
         p1+=1
-        p2+=1
 
 print("1:",p1)
 print("2:",p2)
