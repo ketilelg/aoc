4b1bad06e36951c409143487055de19f4ea7868d
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2025-12-03 07:35:14 +0100

    day 3

--- 2025/03/solve.py
+++ 2025/03/solve.py
@@ -8,7 +8,21 @@ with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
 p1=p2=0
 
 for l in inp:
-    print("l":l)
+    sifre=list(map(int,list(l)))
+    s1=max(sifre[:-1])
+    s1pos=sifre.index(s1)
+    s2=max(sifre[s1pos+1:])
+    p1+=(s1*10)+s2
+
+    p2v=0    
+    spos=0
+    for i in range(11,-1,-1):
+        epos=len(sifre)-i
+        sif=(max(sifre[spos:epos]))
+        p2v*=10
+        p2v+=sif
+        spos=sifre[spos:].index(sif)+1+spos
+    p2+=p2v
 
 print("1:",p1)
 print("2:",p2)
