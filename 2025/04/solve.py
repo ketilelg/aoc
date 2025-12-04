c7ee19459395e5a8ac3523e5e7db0d90a571450c
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2025-12-04 06:37:12 +0100

    day 4

--- 2025/04/solve.py
+++ 2025/04/solve.py
@@ -1,14 +1,56 @@
 import sys
 from collections import defaultdict
 import re
+from copy import deepcopy
 
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     inp = f.read().strip().split("\n")
 
 p1=p2=0
 
+fmap=[]
 for l in inp:
-    print("l",l)
+    fmap.append(list(l))
+
+h=len(fmap)
+w=len(fmap[0])    
+
+def printmap(nmap):
+    for y in range(len(nmap)):
+        for x in range(len(nmap[0])):
+            print(nmap[y][x],end="")
+        print("-")
+
+for x in range(w):
+    for y in range(h):
+        if fmap[y][x]=="@":
+            nneig=0
+            for oy in range(-1,2):
+                for ox in range(-1,2):
+                    if x+ox>=0 and x+ox<w and y+oy>=0 and y+oy<h:
+                        if fmap[y+oy][x+ox]=="@":
+                            nneig+=1
+            if nneig<5:
+                p1+=1
+
+np2=1
+newmap=deepcopy(fmap)
+while np2!=p2:
+    np2=p2
+#    printmap(newmap)
+    for x in range(w):
+        for y in range(h):
+            if fmap[y][x]=="@":
+                nneig=0
+                for oy in range(-1,2):
+                    for ox in range(-1,2):
+                        if x+ox>=0 and x+ox<w and y+oy>=0 and y+oy<h:
+                            if fmap[y+oy][x+ox]=="@":
+                                nneig+=1
+                if nneig<5:
+                    p2+=1
+                    newmap[y][x]="."
+    fmap=deepcopy(newmap)
 
 print("1:",p1)
 print("2:",p2)
