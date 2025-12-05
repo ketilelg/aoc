1a14d695b8279559720c58c9e03c15d9537bb9e5
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2025-12-05 08:28:12 +0100

    day 5

---
+++ 2025/05/solve.py
@@ -0,0 +1,46 @@
+import sys
+from collections import defaultdict
+import re
+
+with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
+    inp = f.read().strip().split("\n\n")
+
+fresh=inp[0].split("\n")
+avail=map(int,inp[1].split("\n"))
+
+p1=p2=0
+
+for i in avail:
+    ff=False
+    for f in fresh:
+        s,e=map(int,f.split("-"))
+        if (i >=s) and (i <=e):
+            ff=ff or True
+    if ff:
+        p1+=1
+
+nf=[] # har aldri overlapp
+for f in fresh: #legg til en og en, men ikke så det blir overlapp:
+    s,e=map(int,f.split("-"))
+    nnf=[]
+    for i in range(len(nf)):
+        (ns,ne)=nf[i]
+        if (e<ns) or (s>ne): #de overlapper ikke
+            pass
+        elif (s<=ns) and (e>=ne): #den nye er større enn den gamle
+            nf[i]=(-1,-1)
+        elif (s>=ns) and (e<=ne): #den gamle er større enn den nye
+            s=e=-1
+        elif (s<=ns) and (e>=ns): #overlapp 
+            e=ns-1
+        elif (s>=ns) and (e>=ne): #overlapp
+            s=ne+1
+    if (s>=0) and (e>=0):
+        nf.append((s,e))
+
+print("nf",nf)
+for t in nf:
+    if (t[1]>=0) and (t[0]>=0):
+        p2+=t[1]-t[0]+1
+print("1:",p1)
+print("2:",p2)
