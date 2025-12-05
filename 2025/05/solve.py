424909febc9fdfe00d273d7e128b5f10dc3c9658
Ketil Kirkerud Elgethun <ketilelg@kraftcert.no>
2025-12-05 12:51:39 +0100

    some cleanup

--- 2025/05/solve.py
+++ 2025/05/solve.py
@@ -1,6 +1,4 @@
 import sys
-from collections import defaultdict
-import re
 
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
     inp = f.read().strip().split("\n\n")
@@ -10,20 +8,22 @@ avail=map(int,inp[1].split("\n"))
 
 p1=p2=0
 
+
 for i in avail:
     ff=False
     for f in fresh:
         s,e=map(int,f.split("-"))
         if (i >=s) and (i <=e):
-            ff=ff or True
+            ff=True
+            break
     if ff:
         p1+=1
 
-nf=[] # har aldri overlapp
+nf=[] # ny, forbedret liste. har aldri overlapp
 for f in fresh: #legg til en og en, men ikke så det blir overlapp:
-    s,e=map(int,f.split("-"))
+    s,e=map(int,f.split("-")) #plukk et nytt intervall fra original liste
     nnf=[]
-    for i in range(len(nf)):
+    for i in range(len(nf)): #sjekk det nye med allerede lagrede intervaller i den nye liste
         (ns,ne)=nf[i]
         if (e<ns) or (s>ne): #de overlapper ikke
             pass
@@ -31,6 +31,7 @@ for f in fresh: #legg til en og en, men ikke så det blir overlapp:
             nf[i]=(-1,-1)
         elif (s>=ns) and (e<=ne): #den gamle er større enn den nye
             s=e=-1
+            break
         elif (s<=ns) and (e>=ns): #overlapp 
             e=ns-1
         elif (s>=ns) and (e>=ne): #overlapp
@@ -38,9 +39,9 @@ for f in fresh: #legg til en og en, men ikke så det blir overlapp:
     if (s>=0) and (e>=0):
         nf.append((s,e))
 
-print("nf",nf)
 for t in nf:
     if (t[1]>=0) and (t[0]>=0):
         p2+=t[1]-t[0]+1
+        
 print("1:",p1)
 print("2:",p2)
