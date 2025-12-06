ad16e6eb1ed7c80a9d4c10caa17f8071722dabd9
Ketil Kirkerud Elgethun <ketil@elgethun.no>
2025-12-06 09:37:31 +0100

    day6, pt1+2

--- 2025/06/solve.py
+++ 2025/06/solve.py
@@ -1,14 +1,49 @@
 import sys
-from collections import defaultdict
-import re
+import numpy
 
 with open(sys.argv[1] if (len(sys.argv) == 2) else 'input') as f:
-    inp = f.read().strip().split("\n")
+    inp = f.read().split("\n")
 
 p1=p2=0
 
-for l in inp:
-    print("l",l)
+inp.pop()
+
+sheet=[]
+for l in inp[:-1]:
+    sheet.append(list(map(int,l.split())))
+sheet.append(inp[-1].split())
+
+for i in range(len(sheet[0])):
+    v=sheet[0][i]
+    op=sheet[-1][i]
+    for j in range(1,len(sheet)-1):
+        if op=="+":
+            v+=sheet[j][i]
+        else: #op=="*"
+            v*=sheet[j][i]
+    p1+=v
+
+#del 2. se på tabellen som chars, slice den nedover
+sh2=numpy.array(list(list(x) for x in inp))
+
+sv=0
+op=""
+for i in range(len(sh2[0])):
+    ss=sh2[0:len(sh2),i]
+    if "".join(ss[:-1]).strip() != "":
+        v=int("".join(ss[:-1]))
+        if ss[-1]!=" ":
+            op=ss[-1]
+            p2+=sv
+            if op=="+":
+                sv=0
+            else:
+                sv=1
+        if op=="+":
+            sv+=v
+        else:
+            sv*=v
+p2+=sv
 
 print("1:",p1)
 print("2:",p2)
